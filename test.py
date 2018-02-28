dba906a3ef4e85c25d633dac03115955276d33cd
管冲 <502187489@qq.com>
2018-02-28 23:27:21 +0800

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-file=r"C:\Users\guan\Desktop\data\news12g_bdbk20g_nov90g_dim128\news12g_bdbk20g_nov90g_dim128.bin"
-import codecs
-import numpy as np
-
-a=np.zeros((3,4))
-b=a[0:2]
-print(b.shape)
-
